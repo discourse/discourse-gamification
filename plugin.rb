6b5766515bc472103e2cbcb61a2f248cd156ae71
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-03-14 10:32:18 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# name: discourse-plugin-name
+# about: TODO
+# version: 0.0.1
+# authors: Discourse
+# url: TODO
+# required_version: 2.7.0
+# transpile_js: true
+
+enabled_site_setting :plugin_name_enabled
+
+after_initialize do
+end
