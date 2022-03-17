4c83ee5920b92a848a9bdd980edcddaea2ed4561
Rafael dos Santos Silva <xfalcox@gmail.com>
2022-03-17 16:16:05 -0300

    FIX: Fix intraday update

--- app/models/gamification_score.rb
+++ app/models/gamification_score.rb
@@ -4,48 +4,51 @@ class DiscourseGamification::GamificationScore < ::ActiveRecord::Base
   attr_reader :user_id, :day, :score
   self.table_name = 'gamification_scores'
 
-  def self.calculate_scores(for_date: Date.today)
+  def self.calculate_scores(since_date: Date.today)
     like_val = SiteSetting.like_score_value
     solution_val = SiteSetting.solution_score_value
-    DB.exec(<<~SQL, for_date: for_date, like_val: like_val, solution_val: solution_val)
-      WITH x AS (SELECT
-        source.user AS user_id,
-        SUM(points) AS p_sum
-      FROM
-      (
-      (
-        SELECT
-          p.user_id AS user,
-          COUNT(*) * :like_val AS points
-        FROM post_actions pa
-        LEFT JOIN posts p ON p.id = pa.post_id
-        WHERE post_action_type_id = 2
-        AND pa.created_at >= :for_date
-        GROUP BY 1
-      )
-      UNION ALL
-      (
-      SELECT
-        posts.user_id AS user_id,
-        COUNT(topic_custom_fields.topic_id) * :solution_val AS points
-      FROM
-        topic_custom_fields
-      INNER JOIN
-        topics ON topic_custom_fields.topic_id = topics.id
-      INNER JOIN
-        posts ON posts.id = topic_custom_fields.value::INTEGER
-      WHERE
-        topic_custom_fields.name = 'accepted_answer_post_id'
-        AND topic_custom_fields.updated_at >= :for_date
-      GROUP BY 1
-      )
+
+    DB.exec(<<~SQL, since_date: since_date, like_val: like_val, solution_val: solution_val)
+      INSERT INTO gamification_scores (user_id, date, score)
+      SELECT user_id, date, SUM(points) AS score
+      FROM (
+        (
+          SELECT
+              p.user_id AS user_id,
+              date_trunc('day', pa.created_at) AS date,
+              COUNT(*) * :like_val AS points
+            FROM
+              post_actions AS pa
+            INNER JOIN posts AS p
+              ON p.id = pa.post_id
+            WHERE
+              post_action_type_id = 2 AND
+              pa.created_at >= :since_date
+            GROUP BY
+            1, 2
+        )
+          UNION ALL
+        (
+            SELECT
+              posts.user_id AS user_id,
+              date_trunc('day', topic_custom_fields.updated_at) AS date,
+              COUNT(topic_custom_fields.topic_id) * :solution_val AS points
+            FROM
+              topic_custom_fields
+            INNER JOIN topics
+              ON topic_custom_fields.topic_id = topics.id
+            INNER JOIN posts
+              ON posts.id = topic_custom_fields.value::INTEGER
+            WHERE
+              topic_custom_fields.name = 'accepted_answer_post_id' AND
+              topic_custom_fields.updated_at >= :since_date
+            GROUP BY
+              1, 2
+        )
       ) AS source
-      GROUP BY 1
-      )
-      INSERT INTO gamification_scores (user_id, score)
-          SELECT * FROM x
-          ON CONFLICT (user_id) DO UPDATE SET score = EXCLUDED.score
+      GROUP BY 1, 2
+      ON CONFLICT (user_id, date) DO UPDATE
+      SET score = EXCLUDED.score
     SQL
   end
 end
-
