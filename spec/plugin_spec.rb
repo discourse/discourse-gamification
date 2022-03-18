b6a093ad0bb65d563c83084935ed17bea81a8fd2
Isaac Janzen <isaac.janzen@discourse.org>
2022-03-18 15:34:41 -0500

    fix spec

--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -6,7 +6,7 @@ describe ::DiscourseGamification do
   let(:user) { Fabricate(:user) }
   let!(:gamification_score) { Fabricate(:gamification_score, user_id: user.id) }
 
-  it "adds discourse_score to the UserCardSerializer" do
+  it "adds gamification_score to the UserCardSerializer" do
     serializer = UserCardSerializer.new(user)
     expect(serializer).to respond_to(:gamification_score)
     expect(serializer.gamification_score).to eq(gamification_score.score)
