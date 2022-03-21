cefe64d135479c031bdcce4c2ebecdce4023857e
Isaac Janzen <isaac.janzen@discourse.org>
2022-03-21 15:18:07 -0500

    lint

--- test/javascripts/acceptance/gamification-score-test.js
+++ test/javascripts/acceptance/gamification-score-test.js
@@ -1,31 +1,32 @@
-import { acceptance, exists, query } from "discourse/tests/helpers/qunit-helpers";
+import {
+  acceptance,
+  exists,
+  query,
+} from "discourse/tests/helpers/qunit-helpers";
 import { click, visit } from "@ember/test-helpers";
-import User from "discourse/models/user";
 import { test } from "qunit";
 import userFixtures from "discourse/tests/fixtures/user-fixtures";
 import { cloneJSON } from "discourse-common/lib/object";
 
-acceptance("Discourse Gamification | User Card | Show Gamification Score", function (needs) {
-  needs.user();
-  needs.pretender((server, helper) => {
-    const cardResponse = cloneJSON(userFixtures["/u/charlie/card.json"]);
-    cardResponse.user.gamification_score = 10
-    server.get("/u/charlie/card.json", () => helper.response(cardResponse));
-  });
+acceptance(
+  "Discourse Gamification | User Card | Show Gamification Score",
+  function (needs) {
+    needs.user();
+    needs.pretender((server, helper) => {
+      const cardResponse = cloneJSON(userFixtures["/u/charlie/card.json"]);
+      cardResponse.user.gamification_score = 10;
+      server.get("/u/charlie/card.json", () => helper.response(cardResponse));
+    });
 
-  test("user card gamification score - score is present", async function (assert) {
-    await visit("/t/internationalization-localization/280");
-    await click('a[data-user-card="charlie"]');
+    test("user card gamification score - score is present", async function (assert) {
+      await visit("/t/internationalization-localization/280");
+      await click('a[data-user-card="charlie"]');
 
-    debugger
-    assert.ok(
-      exists(".user-card .gamification-score"),
-      "score is present"
-
-    );
-    assert.ok(
-      query(".user-card .gamification-score").innerText.includes("10"),
-      "user card has gamification score"
-    );
-  });
-});
+      assert.ok(exists(".user-card .gamification-score"), "score is present");
+      assert.ok(
+        query(".user-card .gamification-score").innerText.includes("10"),
+        "user card has gamification score"
+      );
+    });
+  }
+);
