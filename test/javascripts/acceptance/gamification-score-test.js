5a52884e43fd4cc9f4ed148bff5b3f413b3659ee
Isaac Janzen <isaac.janzen@discourse.org>
2022-03-22 07:26:37 -0500

    user profile wip

--- test/javascripts/acceptance/gamification-score-test.js
+++ test/javascripts/acceptance/gamification-score-test.js
@@ -3,6 +3,7 @@ import {
   exists,
   query,
 } from "discourse/tests/helpers/qunit-helpers";
+import { fixturesByUrl } from "discourse/tests/helpers/create-pretender";
 import { click, visit } from "@ember/test-helpers";
 import { test } from "qunit";
 import userFixtures from "discourse/tests/fixtures/user-fixtures";
@@ -27,6 +28,29 @@ acceptance(
         query(".user-card .gamification-score").innerText.includes("10"),
         "user card has gamification score"
       );
+    });
+  }
+);
+
+acceptance(
+  "Discourse Gamification | User Profile | Show Gamification Score",
+  function (needs) {
+    needs.user();
+    needs.pretender((server, helper) => {
+      const profileResponse = cloneJSON(
+        fixturesByUrl["/u/charlie/summary.json"]
+      );
+      profileResponse.user_summary.gamification_score = 10;
+
+      server.get("/u/charlie/summary.json", () =>
+        helper.response(profileResponse)
+      );
+    });
+
+    test("user profile gamification score - score is present", async function (assert) {
+      await visit("/u/charlie/summary");
+
+      // find summary value here
     });
   }
 );
