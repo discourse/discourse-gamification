bc9325c9f5c7fab6c2ad8aea7cdda2383102cd98
Isaac Janzen <50783505+janzenisaac@users.noreply.github.com>
2022-04-08 12:12:56 -0500

    update leaderboard (#9)

--- assets/javascripts/discourse/routes/gamification-leaderboard-by-name.js
+++ assets/javascripts/discourse/routes/gamification-leaderboard-by-name.js
@@ -5,7 +5,6 @@ export default DiscourseRoute.extend({
   model(params) {
     return ajax(`/leaderboard/${params.leaderboardId}`)
       .then((scores) => {
-        scores.users = scores.users?.sortBy("total_score").reverse();
         return scores;
       })
       .catch(() => this.replaceWith("/404"));
