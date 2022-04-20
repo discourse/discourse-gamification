d1f20f55067ace961eedd4291cd3d2757679eade
Chapoi <101828855+Chapoi@users.noreply.github.com>
2022-04-20 19:31:00 +0100

    FEATURE: Add leaderboard styling (#14)
    Co-authored-by: Joffrey JAFFEUX <j.jaffeux@gmail.com>
    Co-authored-by: Rafael dos Santos Silva <xfalcox@gmail.com>

--- assets/javascripts/discourse/components/gamification-leaderboard.js
+++ assets/javascripts/discourse/components/gamification-leaderboard.js
@@ -1,5 +1,35 @@
 import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import { action } from "@ember/object";
+import showModal from "discourse/lib/show-modal";
 
 export default Component.extend({
   tagName: "",
+
+  @discourseComputed("model.users.[]")
+  currentUserRanking(users) {
+    const user = users.findBy("id", this.currentUser?.id);
+    return user
+      ? {
+          user,
+          total_score: user.total_score,
+          position: users.indexOf(user) + 1,
+        }
+      : null;
+  },
+
+  @discourseComputed("model.users")
+  winners(users) {
+    return users.slice(0, 3);
+  },
+
+  @discourseComputed("model.users.[]")
+  ranking(users) {
+    return users.slice(3);
+  },
+
+  @action
+  showLeaderboardInfo() {
+    showModal("leaderboard-info");
+  },
 });
