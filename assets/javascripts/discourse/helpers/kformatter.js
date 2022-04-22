3816f06ae0065b5224b7ada02772c6b0c8d6e3dc
chapoi <charlie@discourse.org>
2022-04-22 14:18:12 +0100

    Fix: Use correct helper

--- assets/javascripts/discourse/helpers/kformatter.js
+++
@@ -1,9 +0,0 @@
-import Helper from "@ember/component/helper";
-
-export function kformatter(num) {
-  return Math.abs(num) > 9999
-    ? (Math.abs(num) / 1000).toFixed(1).toLocaleString(undefined) + "k"
-    : Math.abs(num);
-}
-
-export default Helper.helper(kformatter);
