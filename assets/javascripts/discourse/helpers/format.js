1cd0efa2f0596050396be95c5e6f63c6940e6c18
Chapoi <101828855+Chapoi@users.noreply.github.com>
2022-04-25 22:27:23 +0100

    Update assets/javascripts/discourse/helpers/format.js
    Co-authored-by: Penar Musaraj <pmusaraj@gmail.com>

--- assets/javascripts/discourse/helpers/format.js
+++ assets/javascripts/discourse/helpers/format.js
@@ -1,7 +1,7 @@
 import Helper from "@ember/component/helper";
 
 export function format(number) {
-  return number.toLocaleString(undefined, {});
+  return I18n.toNumber(number, { precision: 0 });
 }
 
 export default Helper.helper(format);
