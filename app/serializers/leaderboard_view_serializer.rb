8c6a016820166b558749305e098172271e00410e
Isaac Janzen <isaac.janzen@discourse.org>
2022-06-15 16:02:39 -0500

    Always display personal score

--- app/serializers/leaderboard_view_serializer.rb
+++ app/serializers/leaderboard_view_serializer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class LeaderboardViewSerializer < ApplicationSerializer
+  attributes :personal
+
   has_one :leaderboard, serializer: LeaderboardSerializer, embed: :objects
   has_many :users, serializer: UserScoreSerializer, embed: :objects
 
@@ -9,6 +11,10 @@ class LeaderboardViewSerializer < ApplicationSerializer
   end
 
   def users
-    DiscourseGamification::GamificationLeaderboard.scores_for(object[:leaderboard].id, object[:page])
+    DiscourseGamification::GamificationLeaderboard.scores_for(object[:leaderboard].id, page: object[:page])
+  end
+
+  def personal
+    DiscourseGamification::GamificationLeaderboard.scores_for(object[:leaderboard].id, for_user_id: object[:for_user_id])
   end
 end
