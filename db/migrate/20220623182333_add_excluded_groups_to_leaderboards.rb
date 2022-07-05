2df00c58543a9cce9bcb422aa0575a8a11986679
Tim Santos <42608459+timmsantos@users.noreply.github.com>
2022-07-05 19:56:10 +1000

    DEV: Change migration version to a stable-compatible version (#39)

--- db/migrate/20220623182333_add_excluded_groups_to_leaderboards.rb
+++ db/migrate/20220623182333_add_excluded_groups_to_leaderboards.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-class AddExcludedGroupsToLeaderboards < ActiveRecord::Migration[7.0]
+class AddExcludedGroupsToLeaderboards < ActiveRecord::Migration[6.1]
   def change
     add_column :gamification_leaderboards, :excluded_groups_ids, :integer, array: true, null: false, default: []
   end
