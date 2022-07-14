bd8023e8baa58541a2d318a20989a9b03e9d1167
Rafael dos Santos Silva <xfalcox@gmail.com>
2022-07-14 11:45:48 -0300

    FEATURE: Time period filter on leaderboard view (#43)
    * FEATURE: Time period filter on leaderboard view
    
    Co-authored-by: chapoi <charlie@discourse.org>

--- app/controllers/gamification_leaderboard_controller.rb
+++ app/controllers/gamification_leaderboard_controller.rb
@@ -6,6 +6,15 @@ class DiscourseGamification::GamificationLeaderboardController < ::ApplicationCo
     default_leaderboard_id = DiscourseGamification::GamificationLeaderboard.first.id
     params[:id] ||= default_leaderboard_id
     leaderboard = DiscourseGamification::GamificationLeaderboard.find(params[:id])
+    period =
+      case params[:period]&.to_sym
+      when :yearly    then 1.year.ago
+      when :monthly   then 1.month.ago
+      when :quarterly then 3.months.ago
+      when :weekly    then 1.week.ago
+      when :daily     then 1.day.ago
+        else nil
+      end
 
     if !current_user&.staff? && leaderboard.visible_to_groups_ids.present? && (leaderboard.visible_to_groups_ids & current_user&.group_ids).empty?
       raise Discourse::NotFound
@@ -14,6 +23,7 @@ class DiscourseGamification::GamificationLeaderboardController < ::ApplicationCo
         leaderboard: leaderboard,
         page: params[:page].to_i,
         for_user_id: current_user&.id,
+        period: period
       }, LeaderboardViewSerializer, root: false)
     end
   end
