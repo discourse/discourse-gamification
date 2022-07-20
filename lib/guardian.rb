802bff8e7e674caf642101a40ce1abaea2599adb
Rafael dos Santos Silva <xfalcox@gmail.com>
2022-07-20 19:50:55 -0300

    fix guardian check

--- lib/guardian.rb
+++ lib/guardian.rb
@@ -5,7 +5,7 @@ module DiscourseGamification
     def can_see_leaderboard?(leaderboard)
       return true if leaderboard.visible_to_groups_ids.empty?
       return true if self.is_admin?
-      return false if self.user && (leaderboard.visible_to_groups_ids & self.user.group_ids).empty?
+      return true if self.user && !(leaderboard.visible_to_groups_ids & self.user.group_ids).empty?
 
       false
     end
