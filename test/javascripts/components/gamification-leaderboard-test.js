73236a8dfdd14c288dc9012bad5a6ee0c76b1f5e
Guhyoun Nam <rngus2344@gmail.com>
2023-05-25 18:48:51 -0500

    Added tests for components

---
+++ test/javascripts/components/gamification-leaderboard-test.js
@@ -0,0 +1,36 @@
+import { setupRenderingTest } from "discourse/tests/helpers/component-test";
+import { query } from "discourse/tests/helpers/qunit-helpers";
+import hbs from "htmlbars-inline-precompile";
+import { module, test } from "qunit";
+import { render } from "@ember/test-helpers";
+
+function displayName() {
+  return query(".winner__name").innerText.trim();
+}
+
+module(
+  "Discourse Gamification | Component | gamification-leaderboard",
+  function (hooks) {
+    setupRenderingTest(hooks);
+
+    test("name", async function (assert) {
+      this.siteSettings.prioritize_username_in_ux = false;
+      this.set("winner", { username: "id", name: "bob" });
+      this.set("model", { leaderboard: "", personal: "", users: [this.winner] });
+
+      await render(hbs`<GamificationLeaderboard @model={{this.model}} />`);
+
+      assert.strictEqual(displayName(), "bob");
+    });
+
+    test("username", async function (assert) {
+      this.siteSettings.prioritize_username_in_ux = true;
+      this.set("winner", { username: "id", name: "bob" });
+      this.set("model", { leaderboard: "", personal: "", users: [this.winner] });
+
+      await render(hbs`<GamificationLeaderboard @model={{this.model}} />`);
+
+      assert.strictEqual(displayName(), "id");
+    });
+  }
+);
