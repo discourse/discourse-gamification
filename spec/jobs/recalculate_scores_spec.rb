7fd3a13a5ad0eb655c408af6c08561a1699b4e7f
Guhyoun Nam <rngus2344@gmail.com>
2023-07-24 12:52:14 -0500

    FIxed failing backend test

--- spec/jobs/recalculate_scores_spec.rb
+++ spec/jobs/recalculate_scores_spec.rb
@@ -3,13 +3,18 @@
 require "rails_helper"
 
 describe Jobs::RecalculateScores do
+  fab!(:current_user) { Fabricate(:admin) }
+
   before { RateLimiter.enable }
 
   it "publishs MessageBus and executes job" do
     since = 10.days.ago
     DiscourseGamification::GamificationScore.expects(:calculate_scores).with(since_date: since)
 
-    MessageBus.expects(:publish).with("/recalculate_scores", { success: true, remaining: 5 }).once
-    Jobs::RecalculateScores.new.execute(since: since)
+    MessageBus
+      .expects(:publish)
+      .with("/recalculate_scores", { success: true, remaining: 5, user_id: [current_user.id] })
+      .once
+    Jobs::RecalculateScores.new.execute(since: since, user_id: current_user.id)
   end
 end
